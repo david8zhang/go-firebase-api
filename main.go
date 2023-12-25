ad330ea732118dbd6fb16f85c5d4f4dde30925d7
David Zhang <david8zhang@berkeley.edu>
2023-12-25 10:50:32 -0800

    refactored API routes, added complete todo route

--- main.go
+++ main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"os"
 
 	firebase "firebase.google.com/go/v4"
 	"github.com/david8zhang/go-firebase/routes"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
 )
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+			c.Header("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Credentials", "true")
+			c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+			c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+			if c.Request.Method == "OPTIONS" {
+					c.AbortWithStatus(204)
+					return
+			}
+
+			c.Next()
+	}
+}
 
 func main() {
 	r := gin.Default()
@@ -16,10 +35,20 @@ func main() {
 	conf := &firebase.Config{
 		DatabaseURL: "https://habit-rank-default-rtdb.firebaseio.com",
 	}
-
-	app, err := firebase.NewApp(ctx, conf)
-	if err != nil {
-		log.Fatalln("error in initializing firebase app: ", err)
+	
+	var app *firebase.App
+	var err error
+	if _, err := os.Stat("habit-rank-firebase-adminsdk-8xo36-576cec4f1c.json"); errors.Is(err, os.ErrNotExist) {
+		app, err = firebase.NewApp(ctx, conf)
+		if err != nil {
+			log.Fatalln("error in initializing firebase app: ", err)
+		}
+	} else {
+		opt := option.WithCredentialsFile("habit-rank-firebase-adminsdk-8xo36-576cec4f1c.json")
+		app, err = firebase.NewApp(ctx, conf, opt)
+		if err != nil {
+			log.Fatalln("error in initializing firebase app: ", err)
+		}
 	}
 
 	client, err := app.Database(ctx)
@@ -27,9 +56,8 @@ func main() {
 		log.Fatalln("error in creating firebase db client: ", err)
 	}
 
-	routes.CreateTodo(r, client)
-	routes.GetTodos(r, client)
-	routes.DeleteTodo(r, client)
+	r.Use(CORSMiddleware())
+	routes.SetupRoutes(r, client)
 
 	port := os.Getenv("PORT")
 	if port == "" {
---
+++ routes/todos.go
@@ -0,0 +1,111 @@
+package routes
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"strconv"
+
+	"firebase.google.com/go/v4/db"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/exp/maps"
+)
+
+type Todo struct {
+	Id string
+	Title string
+	Difficulty string
+	LastUpdated int64
+}
+
+func SetupRoutes(r *gin.Engine, client *db.Client) {
+	getTodos(r, client)
+	completeTodos(r, client)
+	updateTodo(r, client)
+	createTodo(r, client)
+	deleteTodo(r, client)
+}
+
+func getTodos(r *gin.Engine, client *db.Client) {
+	r.GET("todo", func (c *gin.Context) {
+
+		ref := client.NewRef("todos")
+		var todos map[string]Todo
+		if err := ref.Get(context.TODO(), &todos); err != nil {
+			log.Fatalln("error in reading from firebase DB: ", err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"todos": maps.Values(todos),
+		})
+	})
+}
+
+func completeTodos(r *gin.Engine, client *db.Client) {
+	r.POST("todo/complete", func (c* gin.Context) {
+		id := c.PostForm("id")
+		lastUpdatedStr := c.PostForm("lastUpdated")
+		lastUpdatedVal, err := strconv.ParseInt(lastUpdatedStr, 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ref := client.NewRef("todos/" + id)
+		newTodo := map[string]interface{}{"id":id,"lastUpdated":lastUpdatedVal}
+		if updateErr := ref.Update(context.TODO(), newTodo); updateErr != nil {
+			log.Fatal(updateErr)
+		}
+		c.JSON(http.StatusCreated, gin.H{
+			"updated": id,
+		})
+	})
+}
+
+func updateTodo(r *gin.Engine, client *db.Client) {
+	r.POST("todo/update", func (c *gin.Context) {
+		id := c.PostForm("id")
+		title := c.PostForm("title")
+		difficulty := c.PostForm("difficulty")
+
+		ref := client.NewRef("todos/" + id)
+		newTodo := map[string]interface{}{"id":id, "title":title, "difficulty":difficulty}
+		if err := ref.Update(context.TODO(), newTodo); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusCreated, gin.H{
+			"updated": id,
+		})
+	})
+}
+
+func createTodo(r *gin.Engine, client *db.Client) {
+	r.POST("todo/new", func (c *gin.Context) {
+		id := c.PostForm("id")
+		title := c.PostForm("title")
+		difficulty := c.PostForm("difficulty")
+
+		ref := client.NewRef("todos/" + id)
+		newTodo := map[string]interface{}{"id":id, "title":title, "difficulty":difficulty,"lastUpdated": -1}
+		if err := ref.Set(context.TODO(), newTodo); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusCreated, gin.H{
+			"created": id,
+		})
+	})
+}
+
+func deleteTodo(r *gin.Engine, client *db.Client) {
+	r.DELETE("todo", func (c *gin.Context) {
+		todoId := c.Query("id")
+		ref := client.NewRef("todos/" + todoId)
+		if err := ref.Delete(context.TODO()); err != nil {
+			log.Fatalln("error in deleting ref: ", err)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"id": todoId,
+		})
+	})
+}
