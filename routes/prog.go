fe68b22c3c779b400e2ca2e48a3f14dbf57f7cd2
David Zhang <david8zhang@berkeley.edu>
2023-12-29 13:29:40 -0800

    added expire todo logic

--- routes/prog.go
+++ routes/prog.go
@@ -12,42 +12,34 @@ import (
 
 type Progression struct {
 	ExpTotal int32 `json:"expTotal"`
-	Rank string `json:"rank"`
+	Rank int32 `json:"rank"`
 }
 
 func SetupProgressionRoutes(r *gin.Engine, client *db.Client) {
-	updateExp(r, client)
-	updateRank(r, client)
+	updateProg(r, client)
 	getProgression(r, client)
 }
 
-func updateExp(r *gin.Engine, client *db.Client) {
-	r.POST("prog/exp", func(c *gin.Context) {
+func updateProg(r *gin.Engine, client *db.Client) {
+	r.POST("prog", func(c *gin.Context) {
 		expTotalStr := c.PostForm("expTotal")
 		expTotalVal, err := strconv.ParseInt(expTotalStr, 10, 32); if err != nil {
 			log.Fatal(err)
 		}
+
+		newRankStr := c.PostForm("newRank")
+		newRank, err := strconv.ParseInt(newRankStr, 10, 32); if err != nil {
+			log.Fatal(err)
+		}
+
 		ref := client.NewRef("prog/")
-		expPayload := map[string]interface{}{"expTotal":expTotalVal}
-		if updateErr := ref.Update(context.TODO(), expPayload); updateErr != nil {
+		progPayload := map[string]interface{}{"expTotal":expTotalVal, "newRank":newRank}
+		if updateErr := ref.Update(context.TODO(), progPayload); updateErr != nil {
 			log.Fatal(updateErr)
 		}
 		c.JSON(http.StatusCreated, gin.H{
 			"newExpValue": expTotalVal,
-		})
-	})
-}
-
-func updateRank(r *gin.Engine, client *db.Client) {
-	r.POST("prog/rank", func(c *gin.Context) {
-		newRank := c.PostForm("newRank")
-		ref := client.NewRef("prog/")
-		rankPayload := map[string]interface{}{"rank":newRank}
-		if updateErr := ref.Update(context.TODO(), rankPayload); updateErr != nil {
-			log.Fatal(updateErr)
-		}
-		c.JSON(http.StatusCreated, gin.H{
-			"newRank": newRank,
+			"newRank":newRank,
 		})
 	})
 }
@@ -58,9 +50,6 @@ func getProgression(r *gin.Engine, client *db.Client) {
 		var prog Progression
 		if err := ref.Get(context.TODO(), &prog); err != nil {
 			log.Fatal(err)
-		}
-		if (prog.Rank == "") {
-			prog.Rank = "Bronze I"
 		}
 		c.JSON(http.StatusOK, prog)
 	})
--- routes/todos.go
+++ routes/todos.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"firebase.google.com/go/v4/db"
 	"github.com/gin-gonic/gin"
@@ -16,6 +17,7 @@ type Todo struct {
 	Title string `json:"title"`
 	Difficulty string `json:"difficulty"`
 	LastUpdated int64 `json:"lastUpdated"`
+	Status int32 `json:"status"`
 }
 
 func SetupTodoRoutes(r *gin.Engine, client *db.Client) {
@@ -24,6 +26,7 @@ func SetupTodoRoutes(r *gin.Engine, client *db.Client) {
 	updateTodo(r, client)
 	createTodo(r, client)
 	deleteTodo(r, client)
+	processExpiredTodos(r, client)
 }
 
 func getTodos(r *gin.Engine, client *db.Client) {
@@ -44,14 +47,13 @@ func getTodos(r *gin.Engine, client *db.Client) {
 func completeTodos(r *gin.Engine, client *db.Client) {
 	r.POST("todo/complete", func (c* gin.Context) {
 		id := c.PostForm("id")
-		lastUpdatedStr := c.PostForm("lastUpdated")
-		lastUpdatedVal, err := strconv.ParseInt(lastUpdatedStr, 10, 64)
-		if err != nil {
+		status, err := strconv.ParseInt(c.PostForm("status"), 10, 32); if err != nil {
 			log.Fatal(err)
 		}
+		lastUpdated := time.Now().UnixMilli()
 
 		ref := client.NewRef("todos/" + id)
-		newTodo := map[string]interface{}{"id":id,"lastUpdated":lastUpdatedVal}
+		newTodo := map[string]interface{}{"id":id,"lastUpdated":lastUpdated,"status":status}
 		if updateErr := ref.Update(context.TODO(), newTodo); updateErr != nil {
 			log.Fatal(updateErr)
 		}
@@ -84,9 +86,18 @@ func createTodo(r *gin.Engine, client *db.Client) {
 		id := c.PostForm("id")
 		title := c.PostForm("title")
 		difficulty := c.PostForm("difficulty")
+		status, parseErr := strconv.ParseInt(c.PostForm("status"), 10, 32); if parseErr != nil {
+			log.Fatal(parseErr)
+		}
 
 		ref := client.NewRef("todos/" + id)
-		newTodo := map[string]interface{}{"id":id, "title":title, "difficulty":difficulty,"lastUpdated": -1}
+		newTodo := map[string]interface{}{
+			"id":id,
+			"title":title,
+			"difficulty":difficulty,
+			"lastUpdated": -1,
+			"status":status,
+		}
 		if err := ref.Set(context.TODO(), newTodo); err != nil {
 			log.Fatal(err)
 		}
@@ -108,4 +119,96 @@ func deleteTodo(r *gin.Engine, client *db.Client) {
 			"id": todoId,
 		})
 	})
+}
+
+func processExpiredTodos(r *gin.Engine, client *db.Client) {
+	/**
+		For every todo:
+			if its status is COMPLETE:
+				- if the lastUpdated timestamp is outside of the boundaries of the current day (12AM PST), mark the todo as INCOMPLETE and update its timestamp
+				- otherwise, do nothing since the todo was completed on the current day
+
+			if its status is INCOMPLETE:
+				- if the lastUpdated timestamp is outside of the boundaries of the current day, then we need to deduct rankExp for it (add its penalty to a total and do a batch update on the current rank and exp total)
+				- otherwise, do nothing since the user still has the current day to complete the todo
+
+
+		For rank penalty processing
+			- Grab the current rank and expTotal
+			- Subtract the penalties from the expTotal. If we go below 0, then we demote the rank
+				- Example: Curr rank: Bronze II, Rank Penalty is -20, curr exp is 10. Final result: Bronze I, curr exp: 90
+				- Example: Curr rank: Bronze III, Rank Penalty is -120, curr exp is 10, final result: Bronze I, curr exp: 90
+	*/
+	r.POST("todo/expire", func (c *gin.Context) {
+		loc, err := time.LoadLocation("America/Los_Angeles")
+		if err != nil {
+			log.Fatal(err)
+		}
+		now := time.Now().In(loc)
+		bod := beginningOfDay(now)
+
+		todoRef := client.NewRef("todos")
+		var todos map[string]Todo
+		if getTodosErr := todoRef.Get(context.TODO(), &todos); getTodosErr != nil {
+			log.Fatalln("error fetching TODOs from firebase DB: ", getTodosErr)
+		}
+		todoArr := maps.Values(todos)
+		todosToUpdate := make(map[string]interface{})
+		var penalties []int
+		var currTimestamp = time.Now().UnixMilli()
+
+		for _, todo := range todoArr {
+			if (todo.LastUpdated < bod.UnixMilli()) {
+				// If todo is incomplete, add a penalty
+				if (todo.Status == 0) {
+					penalties = append(penalties, -10)
+				} else {
+					// Otherwise, change its status to incomplete
+					todo.Status = 0
+				}
+				// Change its last updated so that we don't update this todo again
+				todo.LastUpdated = currTimestamp
+				todosToUpdate[todo.Id] = todo
+			}
+		}
+
+		var prog Progression
+		progRef := client.NewRef("prog")
+		if fetchProgErr := progRef.Get(context.TODO(), &prog); fetchProgErr != nil {
+			log.Fatal(fetchProgErr)
+		}
+
+		currExpTotal := int(prog.ExpTotal)
+		currRank := int(prog.Rank)
+		for _, penalty := range penalties {
+			currExpTotal += penalty
+			if (currExpTotal < 0) {
+				if (currRank == 0) {
+					currExpTotal = 0
+				} else {
+					currRank--
+					currExpTotal += 100
+				}
+			}
+		}
+		progPayload := map[string]interface{}{"newRank":currRank,"expTotal":currExpTotal}
+		if updateErr := progRef.Update(context.TODO(), progPayload); updateErr != nil {
+			log.Fatal(updateErr)
+		}
+
+		if len(todosToUpdate) != 0 {
+			if updateTodoErr := todoRef.Update(context.TODO(), todosToUpdate); updateTodoErr != nil {
+				log.Fatal(updateTodoErr)
+			}
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"beginningOfDay": bod,
+		})
+	})
+}
+
+func beginningOfDay(t time.Time) time.Time {
+	year, month, day := t.Date()
+	return time.Date(year, month, day, 0, 0, 0, 0, t.Location())
 }
